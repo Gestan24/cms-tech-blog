0de462dda52de22c3c082c2a77d5fd696691314f
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-09-20 14:36:00 -0500

    added jawsdb functionality

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize('cms_tech_blog', process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize('cms_tech_blog', process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
+  host: 'localhost',
   port: 3306,
-  logging: false
+  
 });
 
 module.exports = sequelize;
